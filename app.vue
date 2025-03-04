43174032cfcd885257ec4e41b3fe7dfa8c457c98
Alan Yuan <a@ldwid.com>
2025-03-04 18:14:44 +0800

    init

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div>
+      <Button variant="ghost">Click me</Button>
+      <ThemeButton></ThemeButton>
+    </div>
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+  </div>
+</template>
